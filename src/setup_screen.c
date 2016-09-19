bc2546a28a303ef81b08e8d2bd22d5f84f6df9c1
市川 真一 <suigun1000@gmail.com>
2016-09-19 13:21:01 +0900

    無意味な初期値を 0 に修正。/X オプション追加

--- src/setup_screen.c
+++ src/setup_screen.c
@@ -41,7 +41,7 @@ static EFI_STATUS set_variable_u64(CHAR16* variable_name, UINT32 attr, UINT64* d
 
 static bool is_boot_to_fw_ui(void)
 {
-    UINT32 attr = EFI_VARIABLE_BOOTSERVICE_ACCESS | EFI_VARIABLE_RUNTIME_ACCESS;
+    UINT32 attr = 0;
     UINT64 os_indications_supported = 0;
 
     EFI_STATUS status = get_variable_u64(L"OsIndicationsSupported", &attr, &os_indications_supported);
